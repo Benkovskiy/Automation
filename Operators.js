7f7fff12fde450cb6f46a78147ac8ca3b6644940
Benkovskiy <benkovskiy91@gmail.com>
2019-12-24 11:11:11 +0200

    first commit

---
+++ Operators.js
@@ -0,0 +1,8 @@
+/********** Arithmetic Operators ***********/
+
+let x = 5;
+let y = 2;
+
+let result = x + y;
+console.log('Result is : '+result)
+
---
+++ comments&variables.js
@@ -0,0 +1,23 @@
+// This is a single comment
+
+/*
+This is a multiple
+line comment
+comment testing
+*/
+
+/*********** Variables ***************/
+
+let x;
+x = 10;
+
+let x = 10;
+
+let message = 'Hello';
+message = 'Welcome';
+
+let x,y;
+x = 10;
+y = 20;
+
+let x = 10, y = 20;
