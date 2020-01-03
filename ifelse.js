8642990f9fb082351a2fb3fd044865e21c32cf65
Benkovskiy <benkovskiy91@gmail.com>
2020-01-03 17:56:05 +0200

    if\else statements added

---
+++ ifelse.js
@@ -0,0 +1,21 @@
+//Variables block
+
+let age = 20;
+
+/***************** First condition ********************/
+if (age > 20){
+
+    console.log ('Congrats! You are aligible to get a driving license');
+}
+
+/**************** Second condition ********************/
+else if (age == 20){
+
+    console.log ('Congrats! You just get aligible to get driving license. Go and drive!');
+}
+
+/********************* Out of the condition results ****************/
+else {
+
+    console.log ('Sorry! You are not aligible to get a driving license...');
+}
